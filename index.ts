dadf83600c336a99fef336694fdaf123c06aade3
Sachin0496 <sach1n0496as@gmail.com>
2025-11-02 15:59:31 +0530

    added basic depencies and ran index.ts with example script

---
+++ index.ts
@@ -0,0 +1,17 @@
+import { CandlestickApi, IsomorphicFetchHttpLibrary, ServerConfiguration } from "./lighter-sdk-ts/generated";
+const BASE_URL = "https://mainnet.zklighter.elliot.ai"
+const SOL_MARKET_ID = 2
+
+async function getKlines(marketId: number) {
+    const klinesApi = new CandlestickApi({
+        baseServer: new ServerConfiguration<{  }>(BASE_URL, {  }),
+        httpApi: new IsomorphicFetchHttpLibrary(),
+        middleware: [],
+        authMethods: {}
+    });
+
+    const klines = await klinesApi.candlesticks(SOL_MARKET_ID, '5m', Date.now() - 1000 * 60 * 60 * 1, Date.now(), 50, false);
+    console.log(klines);
+}
+
+getKlines(SOL_MARKET_ID);
